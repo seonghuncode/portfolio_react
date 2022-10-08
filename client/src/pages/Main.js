64cbeff65ce0b97cf133ae0815be4826a9ecda7b
seonghuncode <gnos456@naver.com>
2022-10-08 17:53:03 +0900

    서버에서 api받고 객체 정리해서 가공 하기 까지

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faMagnifyingGlassLocation } from "@fortawesome/free-solid-svg-icons";
@@ -11,63 +11,22 @@ function Main() {
   const [apartData, setapartData] = React.useState({});
   const [loading, setLoading] = React.useState(true); //아래서 삼항식을 통해 true이면 로딩화면을 false이면 회면을 보여주기 위한 변수
 
-  //리액트에서 api를 불러오는 방법이 아니라 server에서 불러오는 방법
-  const apartAPIServer = async () => {
+  //리액트에서 api를 불러오는 방법이 아니라 server에서 불러오기
+  const apartAPI = async () => {
     await axios({
       url: "http://localhost:5000/apartAPI",
     })
-      .then((response) => {})
+      .then((response) => {
+        console.log(response);
+      })
       .catch((e) => {
         console.log(e);
       });
   };
 
-  const apartAPI = async () => {
-    //
-    const url =
-      "https://cors-anywhere.herokuapp.com/http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev";
-    const serviceKey =
-      "WM+MJ1skpxpAh/LF6vyhnwEyYL6jE0z2qKopFOydxC8gt8G9cJ9sQror5m99zhcElmmdgcYb/sWQ+6jNqjdGCA==";
-
-    await axios({
-      url: url,
-      params: {
-        serviceKey: serviceKey,
-
-        LAWD_CD: 11110,
-        DEAL_YMD: 201512,
-      },
-    })
-      .then((response) => {
-        // console.log("response정보");
-        // console.log(response);
-        // console.log("response.data정보");
-        // console.log(response.date);
-
-        //JSON데이터로 변환
-        // const jsonData = parseStr(response.data);
-        // console.log("jsonData정보");
-        console.log(response);
-        console.log("0번째 데이터");
-        console.log(response.data.response.body.items.item[0]);
-
-        const data = response.data.response.body.items;
-        setapartData(data);
-        console.log(apartData);
-
-        //바꾼데이터를 변환 시키기
-      })
-      .catch(() => {
-        console.log("apart API 에러!!");
-      });
-
-    setLoading(false);
-  };
   React.useEffect(() => {
     apartAPI();
-  }, []);
-
-  console.log(apartData); //실행 순서는 항상 return을 먼저 보여주고 useEffect를 실행시켜 apartAPI를 실행 시키는 순서이다.
+  });
 
   return (
     <div>
@@ -135,30 +94,28 @@ function Main() {
         </div>
       </div>
       {/* 추천 매물 아래는 공공 데이터를 불러와 뿌려주는 작업 */}
-      {loading ? (
-        <div>로딩중</div>
-      ) : (
-        <div className="bottom-area">
-          <div>
-            <div className="introduce" type="button" value="추천매물">
-              추천매물
-              <button
-                type="button"
-                style={{
-                  backgroundColor: "transparent",
-                  border: "none",
-                }}
-              >
-                <FontAwesomeIcon
-                  style={{ padding: "1px" }}
-                  icon={faMagnifyingGlassLocation}
-                  size="2x"
-                />
-              </button>
-            </div>
+
+      <div className="bottom-area">
+        <div>
+          <div className="introduce" type="button" value="추천매물">
+            추천매물
+            <button
+              type="button"
+              style={{
+                backgroundColor: "transparent",
+                border: "none",
+              }}
+            >
+              <FontAwesomeIcon
+                style={{ padding: "1px" }}
+                icon={faMagnifyingGlassLocation}
+                size="2x"
+              />
+            </button>
           </div>
         </div>
-      )}
+      </div>
+
       {/**추천 매울에 대한 정보를 정리해서 보여준다*/}
       {/* 한번에 가지고 온 50개의 데이터를 5개를 랜덤 숫자를 뽑아 5개의 값을 추천으로 보여 준다. */}
 
